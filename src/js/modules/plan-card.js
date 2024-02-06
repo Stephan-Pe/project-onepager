a4e75b10d7e31481d09e7756b23937240429e21a
Stephan-Pe <st.petersen63@gmail.com>
2024-02-06 16:33:43 +0100

    refactoring js-component

--- src/js/modules/plan-card.js
+++ src/js/modules/plan-card.js
@@ -1,6 +1,13 @@
 class PlanCard extends HTMLElement {
     set planCard(planCard) {
-        this.innerHTML = `<div class="overlay-image"> <button class="btn btn__close" data-btn-close>x</button> <img src="${planCard.image}" alt="${planCard.title}"><h1>${planCard.title}</h1></div>`;
+        this.status = planCard.status;
+        if (this.status > 0) {
+            this.status = "reserviert";
+        } else {
+            this.status = '';
+        }
+
+        this.innerHTML = `<div class="overlay"> <button class="btn btn__close" data-btn-close>x</button><div class="overlay__image"> <img src="${planCard.image}" alt="${planCard.title}"></div><div class="overlay__text"><h3 class="overlay__text--heading">${planCard.title}</h3> <span class="overlay__text--item">Zimmer: ${planCard.rooms}</span> <span class="overlay__text--item">Wohnfläche: ${planCard.size} m<small class="overlay__text--super">2</small></span><p class="overlay__text--desc">${planCard.description}</p><span class="overlay__text--absolute">${this.status}</span></div></div>`;
     }
 }
 customElements.define('plan-card', PlanCard);
--- src/js/modules/planComponent.js
+++ src/js/modules/planComponent.js
@@ -6,15 +6,22 @@ const API_URL = "data/data.json";
 export async function planComponent() {
     try {
         data = await fetchData(API_URL);
-
+        const downloadSection = document.getElementById('plaene');
         let imageContainer = document.querySelector('#plaene');
         let planData = data.images;
-
+        planData.forEach(item => {
+            let el = document.createElement('DIV');
+            el.className = 'download-section__thumb';
+            el.innerHTML = `<h3>${item.rooms} Zimmer-${item.title}</h3><img class="thumbnail"
+            src="${item.image}" alt="${item.title}" data-id="${item.id}">`;
+            downloadSection.appendChild(el);
+        });
 
         imageContainer.addEventListener('click', (e) => {
             planData.forEach(planCard => {
 
                 if (e.target.matches(`[data-id="${planCard.id}"]`)) {
+
                     let old = document.querySelector('.plan-card');
                     if (old) imageContainer.removeChild(old);
                     const el = document.createElement('plan-card');
